d74f3c3dc59185f1b808d79ce3f1a8b87d6103be
Codewrapperz2021 <codewrapperz360@gmail.com>
2021-12-16 21:58:12 +0530

    time table

--- src/component/timetablemanagement/addtimetable.js
+++ src/component/timetablemanagement/addtimetable.js
@@ -17,13 +17,13 @@ export default function Timetable() {
     const [faculties, setFaculties] = useState([]);
     const [days, setDays] = useState([]);
     const [selectcourseID, setSelectcourseID] = useState(0)
-    const [selectsubjectmon, setSelectsubjectmon] = useState(0)
-    const [selectsubjecttue, setSelectsubjecttue] = useState(0)
-    const [selectsubjectwed, setSelectsubjectwed] = useState(0)
-    const [selectsubjectthr, setSelectsubjectthr] = useState(0)
-    const [selectsubjectfri, setSelectsubjectfri] = useState(0)
-    const [selectsubjectsat, setSelectsubjectsat] = useState(0)
-    const [selectsubjectsun, setSelectsubjectsun] = useState(0)
+    const [selectsubjectID, setSelectsubjectID] = useState(0)
+    // const [selectsubjecttue, setSelectsubjecttue] = useState(0)
+    // const [selectsubjectwed, setSelectsubjectwed] = useState(0)
+    // const [selectsubjectthr, setSelectsubjectthr] = useState(0)
+    // const [selectsubjectfri, setSelectsubjectfri] = useState(0)
+    // const [selectsubjectsat, setSelectsubjectsat] = useState(0)
+    // const [selectsubjectsun, setSelectsubjectsun] = useState(0)
  
     console.log(subjects)
 
@@ -31,7 +31,7 @@ export default function Timetable() {
         lectureservices.viewlecture()
             .then(res => {
                 const lectures = res.data;
-                setLecture(lectures);
+                setLecture(lectures);                                         
             })
             dayservices.viewday()
             .then(res => {
@@ -56,10 +56,10 @@ export default function Timetable() {
             })
     }, [])
    
-    function handleSchedule() {
-    //     e.preventDefault();
-    //     const data = {course_name:selectcourse, subject_name:selectsubjectmon,faculty_name:faculties}
-    //     timetableservices.addtimetable(data)
+    function handleSchedule(e) {
+        e.preventDefault();
+        const data = {course_name:selectcourseID, subject_name:selectsubjectID,faculty_name:faculties}
+        timetableservices.addtimetable(data)
      }
     return (
 
@@ -89,83 +89,74 @@ export default function Timetable() {
                                     <tr><td>{lecture.lect_name}</td><td>{lecture.lect_time + '-' + (parseInt(lecture.lect_time) + 1) + ":00"}</td></tr>
                                     
                                     <td>
-                                        <select class="lecture" name="subject" onChange={(e) => setSelectsubjectmon(e.target.value)} >
+                                        <select class="lecture" name="subject" onChange={(e) => setSelectsubjectID(e.target.value)} >
                                             <option  value={0}>Select Subject</option>
                                             {selectcourseID ? (subjects.filter(subject => subject.course_id == selectcourseID)).map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) }) : subjects.map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) })}
                                         </select><br></br><br></br>
                                         <select class="lecture" name="faculty">
                                             <option value={0}>Select Faculty</option>
-                                            {selectsubjectmon ? (faculties.filter(faculty => faculty.subject_id == selectsubjectmon)).map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) }) : faculties.map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
+                                            {faculties.map(faculty => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
                                         </select>
                                     </td>
                                     <td>
-                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjecttue(e.target.value)} >
-                                            <option value={0}>Select Subject</option>
+                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjectID(e.target.value)} >
+                                            <option  value={0}>Select Subject</option>
                                             {selectcourseID ? (subjects.filter(subject => subject.course_id == selectcourseID)).map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) }) : subjects.map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) })}
                                         </select><br></br><br></br>
                                         <select class="lecture" name="faculty">
                                             <option value={0}>Select Faculty</option>
-                                            {selectsubjecttue ? (faculties.filter(faculty => faculty.subject_id == selectsubjecttue)).map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) }) : faculties.map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
+                                            {faculties.map(faculty => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
                                         </select>
                                     </td>
                                     <td>
-                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjectwed(e.target.value)} >
-                                            <option value={0}>Select Subject</option>
+                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjectID(e.target.value)} >
+                                            <option  value={0}>Select Subject</option>
                                             {selectcourseID ? (subjects.filter(subject => subject.course_id == selectcourseID)).map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) }) : subjects.map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) })}
                                         </select><br></br><br></br>
                                         <select class="lecture" name="faculty">
                                             <option value={0}>Select Faculty</option>
-                                            {selectsubjectwed ? (faculties.filter(faculty => faculty.subject_id == selectsubjectwed)).map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) }) : faculties.map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
+                                            {faculties.map(faculty => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
                                         </select>
                                     </td>
                                     <td>
-                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjectthr(e.target.value)} >
-                                            <option value={0}>Select Subject</option>
+                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjectID(e.target.value)} >
+                                            <option  value={0}>Select Subject</option>
                                             {selectcourseID ? (subjects.filter(subject => subject.course_id == selectcourseID)).map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) }) : subjects.map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) })}
                                         </select><br></br><br></br>
                                         <select class="lecture" name="faculty">
                                             <option value={0}>Select Faculty</option>
-                                            {selectsubjectthr ? (faculties.filter(faculty => faculty.subject_id == selectsubjectthr)).map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) }) : faculties.map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
+                                            {faculties.map(faculty => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
                                         </select>
                                     </td>
                                     <td>
-                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjectfri(e.target.value)} >
-                                            <option value={0}>Select Subject</option>
+                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjectID(e.target.value)} >
+                                            <option  value={0}>Select Subject</option>
                                             {selectcourseID ? (subjects.filter(subject => subject.course_id == selectcourseID)).map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) }) : subjects.map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) })}
                                         </select><br></br><br></br>
                                         <select class="lecture" name="faculty">
                                             <option value={0}>Select Faculty</option>
-                                            {selectsubjectfri ? (faculties.filter(faculty => faculty.subject_id == selectsubjectfri)).map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) }) : faculties.map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
+                                            {faculties.map(faculty => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
                                         </select>
                                     </td>
                                     <td>
-                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjectsat(e.target.value)} >
-                                            <option value={0}>Select Subject</option>
+                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjectID(e.target.value)} >
+                                            <option  value={0}>Select Subject</option>
                                             {selectcourseID ? (subjects.filter(subject => subject.course_id == selectcourseID)).map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) }) : subjects.map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) })}
                                         </select><br></br><br></br>
                                         <select class="lecture" name="faculty">
                                             <option value={0}>Select Faculty</option>
-                                            {selectsubjectsat ? (faculties.filter(faculty => faculty.subject_id == selectsubjectsat)).map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) }) : faculties.map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
+                                            {faculties.map(faculty => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
                                         </select>
                                     </td>
-                                    <td>
-                                    <select class="lecture" name="subject" onChange={(e) => setSelectsubjectsun(e.target.value)} >
-                                            <option value={0}>Select Subject</option>
-                                            {selectcourseID ? (subjects.filter(subject => subject.course_id == selectcourseID)).map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) }) : subjects.map((sub) => { return (<option value={sub.id}>{sub.sname}</option>) })}
-                                        </select><br></br><br></br>
-                                        <select class="lecture" name="faculty">
-                                            <option value={0}>Select Faculty</option>
-                                            {selectsubjectsun ? (faculties.filter(faculty => faculty.subject_id == selectsubjectsun)).map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) }) : faculties.map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
-                                        </select>
-                                    </td>
-                                    <td>
-                                    <button id="button" class="Schedule" onClick={handleSchedule}>Schedule</button>
-                                    </td>
+                                   
                                 </tr>
                             )}
                         </tbody> 
                     </table>
-                    </form>
+                    </form>   
+                </div>
+                <div className='text-center'>
+                <button id="button" class="Schedule" onClick={handleSchedule}>Schedule</button>
                 </div>
             </div>
     )
@@ -185,6 +176,10 @@ export default function Timetable() {
         </select>
         </td>)}
         </tr>)} */}
+    //    ` <select class="lecture" name="faculty">
+    //     <option value={0}>Select Faculty</option>
+    //     {selectsubjectsun ? (faculties.filter(faculty => faculty.subject_id == selectsubjectsun)).map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) }) : faculties.map((faculty) => { return (<option value={faculty.first_name}>{faculty.first_name}</option>) })}
+    // </select>`
 
 
 
