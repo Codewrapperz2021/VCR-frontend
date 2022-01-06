dc99c93de0aa2b5408888709e12bbca138946ed6
Shivam <codewrapperz360@gmail.com>
2022-01-06 16:42:40 +0800

    cahbey

--- src/component/masterdas/adminsidebar.js
+++ src/component/masterdas/adminsidebar.js
@@ -82,7 +82,12 @@ export default function Adminsidebar() {
                             <Link className="nav-link" to="/addtimetable">Add Timetable</Link>
                             <Link className="nav-link" to="/viewtimetable">View Timetable</Link>
                         </nav>
+                        
                     </div>
+                    <Link className="nav-link" to="/todolist">
+                        <div className="sb-nav-link-icon  "><i className="fas fa-calendar-check navbar-logo"></i></div>
+                        <h6 className="pt-2 " style={{ color: "white" }}>My Todo list</h6>
+                    </Link>
 
                 </div>
             </div>
